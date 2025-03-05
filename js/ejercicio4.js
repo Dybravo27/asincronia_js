c3bb44105994354b48d6afff4b47ed63011cf6be
Dylan Bravo <dylanslebyng@gmail.com>
2025-03-05 09:11:03 -0500

    ejercicio 4 completado

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -3,3 +3,19 @@
 // argumento
 // • sí dicho callback devuelve true, pushea el elemento a un nuevo array
 // • devuelva el array final con los elementos que pasaron el "filtro".
+
+const filter = function (arreglo, callback) {
+  const nuevoArreglo = [];
+  for (let i = 0; i < arreglo.length; i++) {
+    if (callback(arreglo[i]) > 20) {
+      nuevoArreglo.push(arreglo[i]);
+    }
+  }
+  return nuevoArreglo;
+}
+function multiplicar(numero) {
+  numero *= 2;
+  return numero;
+}
+let arreglo = [1, 2, 45, 30, 5];
+console.log(filter(arreglo, multiplicar));
