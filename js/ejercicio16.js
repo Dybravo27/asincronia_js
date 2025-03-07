79c8328aa0e20dc05d1c5a2f0e0935f7c6452574
Dylan Bravo <dylanslebyng@gmail.com>
2025-03-06 21:55:12 -0500

    ejercicio 16 completado

---
+++ js/ejercicio16.js
@@ -0,0 +1,19 @@
+// Después de realizar una llamada a un archivo local en formato json, utiliza el método then()
+// para filtrar los datos y mostrar solo los elementos que cumplan ciertos criterios (por ejemplo,
+// mostrar solo los nombres que comiencen con "A").
+
+const datosUsuarios = async () => {
+    const datos = await fetch('../js/json/ejercicio16.json')
+    const resultados = await datos.json();
+    return resultados;
+}
+
+fetch('../js/json/ejercicio16.json').then((datos) => {
+    datos.json().then((datosUsuarios) => {
+        for (let i = 0; i < datosUsuarios.length; i++) {
+            if (datosUsuarios[i].nombre.startsWith('A')) {                
+                console.log(datosUsuarios[i].nombre);
+            }
+        }
+    })
+});
